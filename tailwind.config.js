4d59bc39fb88f4d6e421dbe3482d284afbdebe83
zoeg00 <zoeguzman.ok@gmail.com>
2023-11-02 22:40:09 -0300

    agrego postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    color: {
-      1: "#002b40",
-      2: "#004c65",
-      3: "#007a94",
-      4: "#00b5ca",
-      5: "#00ffff"
+    extend: {
+      color: {
+        1: "#002b40",
+        2: "#004c65",
+        3: "#007a94",
+        4: "#00b5ca",
+        5: "#00ffff"
+      },
     },
   },
   plugins: [],
 }
-
